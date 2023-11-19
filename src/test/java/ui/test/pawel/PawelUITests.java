7837d57f0aaf164ba4b11b2a1de62d04b4d92e4f
Paweł Dera <paweldera01@gmail.com>
2023-11-19 16:55:22 +0100

    Imp ui tests

--- src/test/java/ui/test/pawel/PawelUITests.java
+++ src/test/java/ui/test/pawel/PawelUITests.java
@@ -1,65 +1,36 @@
 package ui.test.pawel;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.support.ui.Select;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 
-public class PawelUITests {
+public class PawelUITests extends BaseUITest {
 
-    public static final String WEBDRIVER_LOCATION = "src/test/resources/chromedriver.exe";
-    WebDriver driver;
 
-    @BeforeEach
-    public void setUp() {
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
-        System.setProperty("webdriver.chrome.driver", WEBDRIVER_LOCATION);
-        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-    }
+    public static final String URL_CATEGORIES_ADD = "https://practicesoftwaretesting.com/#/admin/categories/add";
+    public static final String URL_CATEGORIES = "https://practicesoftwaretesting.com/#/admin/categories";
+    public static final String URL_PRODUCT = "https://practicesoftwaretesting.com/#/product/01HFM1H6ZJP3FVZEDMDNHEZ539";
+    public static final String URL_CHECKOUT = "https://practicesoftwaretesting.com/#/checkout";
+    public static final String URL_ORDERS = "https://practicesoftwaretesting.com/#/admin/orders";
+    public static final String URL_CONTACT = "https://practicesoftwaretesting.com/#/contact";
+    public static final String URL_MESSAGES = "https://practicesoftwaretesting.com/#/admin/messages";
 
     @Test
-    public void simpleTest() {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        driver.get("https://practicesoftwaretesting.com/#/auth/login");
+    public void addNewCategoryUITest() {
 
-        WebElement emailInput = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
-        WebElement passwordInput = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
-        WebElement loginButton = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.className("btnSubmit")));
+        loginAsAdmin();
 
-        emailInput.sendKeys("admin@practicesoftwaretesting.com");
-        passwordInput.sendKeys("welcome01");
+        driver.get(URL_CATEGORIES_ADD);
 
-        loginButton.click();
-
-        wait.until(ExpectedConditions.urlContains("#/admin/dashboard"));
-
-        driver.get("https://practicesoftwaretesting.com/#/admin/categories/add");
-
-        WebElement nameInput = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
-        WebElement slugInput = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.id("slug")));
-        WebElement saveButton = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-primary")));
+        WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
+        WebElement slugInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("slug")));
+        WebElement saveButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-primary")));
 
         nameInput.sendKeys("Test Category");
         slugInput.sendKeys("test-category");
@@ -68,15 +39,163 @@ public class PawelUITests {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")));
 
-        driver.get("https://practicesoftwaretesting.com/#/admin/categories");
+        driver.get(URL_CATEGORIES);
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table//tr")));
 
         By tableCol1 = By.xpath("//td[text()='Test Category']");
         By tableCol2 = By.xpath("//td[text()='test-category']");
+
         WebElement row1 = wait.until(ExpectedConditions.visibilityOfElementLocated(tableCol1));
         WebElement row2 = wait.until(ExpectedConditions.visibilityOfElementLocated(tableCol2));
+
         assertTrue(row1.isDisplayed(), "Expected row1 found");
         assertTrue(row2.isDisplayed(), "Expected row2 found");
+    }
+
+    @Test
+    public void addNewOrderUITest() {
+
+        loginAsAdmin();
+
+        driver.get(URL_PRODUCT);
+
+        WebElement addToCartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-add-to-cart")));
+
+        addToCartButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("toast-body")));
+
+        driver.get(URL_CHECKOUT);
+
+        WebElement proceedToCheckoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='proceed-1']")));
+
+        proceedToCheckoutButton.click();
+
+        WebElement proceedToCheckoutButton2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='proceed-2']")));
+
+        proceedToCheckoutButton2.click();
+
+        WebElement addressInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("address")));
+        WebElement cityInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("city")));
+        WebElement stateInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("state")));
+        WebElement countryInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("country")));
+        WebElement postCodeInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("postcode")));
+        WebElement proceedToCheckoutButton3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='proceed-3']")));
+
+        addressInput.clear();
+        addressInput.sendKeys("Test Address");
+        cityInput.clear();
+        cityInput.sendKeys("Test City");
+        stateInput.clear();
+        stateInput.sendKeys("Test State");
+        countryInput.clear();
+        countryInput.sendKeys("Test Country");
+        postCodeInput.clear();
+        postCodeInput.sendKeys("1122AB");
+
+        proceedToCheckoutButton3.click();
+
+        WebElement selectElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("payment-method")));
+        WebElement accountNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("account-name")));
+        WebElement accountNumberInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("account-number")));
+        WebElement confirmButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='finish']")));
+
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("Credit Card");
+
+        accountNameInput.sendKeys("Test Account");
+        accountNumberInput.sendKeys("09876543ABC");
+
+        confirmButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("help-block")));
+
+        confirmButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("order-confirmation")));
+
+        driver.get(URL_ORDERS);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table//tr")));
+
+        By tableCol1 = By.xpath("//td[text()='Test Address']");
+        By tableCol2 = By.xpath("//td[text()='$14.15']");
+
+        WebElement row1 = wait.until(ExpectedConditions.visibilityOfElementLocated(tableCol1));
+        WebElement row2 = wait.until(ExpectedConditions.visibilityOfElementLocated(tableCol2));
+
+        assertTrue(row1.isDisplayed(), "Expected row1 found");
+        assertTrue(row2.isDisplayed(), "Expected row2 found");
+    }
+
+    @Test
+    public void editOrderStatusUITest() {
+
+        loginAsAdmin();
+
+        driver.get(URL_ORDERS);
+
+        WebElement firstRow = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table//tr[2]")));
+        List<WebElement> childElements = firstRow.findElements(By.xpath("./*"));
+
+        if (childElements.get(3).getText().equals("COMPLETED")) {
+            throw new RuntimeException("Order status is COMPLETED");
+        }
+
+        String savedStatus = childElements.get(3).getText();
+
+        childElements.get(5).findElement(By.tagName("a")).click();
+
+        WebElement selectElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='order-status']")));
+        WebElement updateStatus = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='update-status-submit']")));
+
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("COMPLETED");
+        updateStatus.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")));
+
+        driver.get(URL_ORDERS);
+
+        WebElement firstRow2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table//tr[2]")));
+        List<WebElement> childElements2 = firstRow2.findElements(By.xpath("./*"));
+
+        assertEquals(childElements2.get(3).getText(), savedStatus);
+    }
+
+    @Test
+    public void sendContactMessageUITest() {
+
+        final String message = "Test message Test message Test message Test message Test message";
+
+        loginAsAdmin();
+
+        driver.get(URL_CONTACT);
+
+        WebElement selectElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='subject']")));
+        WebElement textArea = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='message']")));
+        WebElement sendButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='contact-submit']")));
+
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("Customer service");
+
+        textArea.sendKeys(message);
+        sendButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")));
+
+        driver.get(URL_MESSAGES);
+
+        WebElement firstRow = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table//tr[2]")));
+        List<WebElement> childElements = firstRow.findElements(By.xpath("./*"));
+
+        childElements.get(4).findElement(By.tagName("a")).click();
+
+        WebElement messageText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("card-text")));
+
+        wait.until(ExpectedConditions.textToBePresentInElement(messageText, message));
+
+        assertEquals(message, messageText.getText());
     }
 }
